e8cd25e931c5fabc4908f0538fdcdcc636cce529
zhuangliang <liang_zhuang_dl@outlook.com>
2018-12-12 14:36:20 +0800

    添加验证码校验方法

--- src/main/java/com/example/captcha/controller/CaptchaController.java
+++ src/main/java/com/example/captcha/controller/CaptchaController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,7 +22,12 @@ public class CaptchaController {
   private CaptchaService captchaService;
 
   @GetMapping(value = "/createCaptcha")
-  public Map<String, String> createCaptcha() throws IOException {
-    return captchaService.createCaptcha();
+  public Map<String, String> createCaptcha(@RequestParam("width") Integer width, @RequestParam("height") Integer height, @RequestParam("number") Integer number) throws IOException {
+    return captchaService.createCaptcha(width, height, number);
+  }
+
+  @PostMapping(value = "/checkCaptcha")
+  public Map<String, Object> checkCaptcha(@RequestParam("token")String token, @RequestParam("captcha")String captcha){
+    return captchaService.checkCaptcha(token, captcha);
   }
 }
--- src/main/java/com/example/captcha/service/CaptchaService.java
+++ src/main/java/com/example/captcha/service/CaptchaService.java
@@ -4,5 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface CaptchaService {
-  Map<String, String> createCaptcha() throws IOException;
+  Map<String, String> createCaptcha(Integer width, Integer height, Integer number) throws IOException;
+
+  Map<String, Object> checkCaptcha(String token, String captcha);
 }
